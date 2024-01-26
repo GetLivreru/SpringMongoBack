255dedca43a76be1966a5e6ce9cda6e83d7c21c2
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-01-26 22:51:30 +0600

    add

--- Server/src/main/java/com/example/SpringMongoProject/SpringMongoProjectApplication.java
+++ Server/src/main/java/com/example/SpringMongoProject/SpringMongoProjectApplication.java
@@ -3,6 +3,8 @@ package com.example.SpringMongoProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class SpringMongoProjectApplication {
 
